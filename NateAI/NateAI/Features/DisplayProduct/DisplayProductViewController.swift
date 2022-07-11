22ac0b966d71cec765815285df7c64dc9a9699e8
Rastislav Smolen <>
2022-07-11 19:46:13 +0200

    fix(*) small cleanup indentation/notes

--- NateAI/NateAI/Features/DisplayProduct/DisplayProductViewController.swift
+++ NateAI/NateAI/Features/DisplayProduct/DisplayProductViewController.swift
@@ -2,29 +2,27 @@
 import UIKit
 
 class DisplayProductViewController: UICollectionViewController {
-    
-    var product: Products?
-    var i = Int()
-    
+
+    //MARK: Variables
+    private var product: Products?
+    private var i = Int()
+    private var model : ProdcutListViewModel?
+
+    //MARK: Constants
+    private let cache = NSCache<NSNumber, UIImage>()
+    private let utilityQueue = DispatchQueue.global(qos: .utility)
     private let sectionInsets = UIEdgeInsets(
         top: 0,
         left: 0,
         bottom: 0,
         right: 0)
-    
-    private var model : ProdcutListViewModel?
-    
-    
-    private let cache = NSCache<NSNumber, UIImage>()
-    private let utilityQueue = DispatchQueue.global(qos: .utility)
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupModel()
         fetchData()
     }
-    
-    
+
     private func setupModel() {
         model = ProdcutListViewModel()
     }
@@ -59,46 +57,45 @@ class DisplayProductViewController: UICollectionViewController {
     private func reload(){
         DispatchQueue.main.async {
             self.collectionView.reloadData()
-            
         }
     }
-    
+
 }
 
 // MARK: - Collection View Setup
 
 extension DisplayProductViewController {
-    
+
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         i = indexPath.row
         performSegue(withIdentifier: "ShowDetail", sender: collectionView)
         
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destinationVC = segue.destination as? ProductDetailViewController else { return }
         destinationVC.product = product
         destinationVC.i = i
     }
-    
+
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
+
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         guard let productCount = product?.products[section].images[section].count else {return 1}
         return productCount
     }
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell else {return UICollectionViewCell()}
-        
+
         cell.layer.borderWidth = 1
         cell.layer.borderColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
         
         let itemNumber = NSNumber(value: indexPath.row)
         
         if let cachedImage = self.cache.object(forKey: itemNumber) {
-            
-            print("Using a cached image for item: \(itemNumber)")
             cell.productImageView.image = cachedImage
             
         } else {
